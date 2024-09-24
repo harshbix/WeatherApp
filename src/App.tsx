1175e50f33d50677e05ba087e0125b138a8fd4cc
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-09-24 16:38:30 +0300

    Update App.tsx
    \

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const App = () => {
   return (
     <div className='container box'>
-      App
+      <div className="box-child bg-success p-3 rounded text-white">Hello, World!</div>
     </div>
   )
 }
